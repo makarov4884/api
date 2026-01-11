c25e26a747641accd48ed68918f2f3600fcd5ec2
[makarov4884] <[scg4884@naver.com]>
2026-01-11 21:23:38 +0900

    Add Dockerfile and update port config

--- test/bcraping-api/main.py
+++ test/bcraping-api/main.py
@@ -425,4 +425,6 @@ async def websocket_monitor(websocket: WebSocket, bj_id: str, station_id: str):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
